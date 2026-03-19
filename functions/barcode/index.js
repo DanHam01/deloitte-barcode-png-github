ce884fc717be8de5ae7ac6552a921f1b9bd0a296
Hamilton <dahamilton@deloitte.com.au>
2026-03-19 19:26:18 +1100

    Test function

--- functions/barcode/index.js
+++ functions/barcode/index.js
@@ -1,29 +1,5 @@
-import bwipjs from 'bwip-js/browser';
-
-export async function onRequestGet({ request }) {
-  const url = new URL(request.url);
-  const text = url.searchParams.get("text");
-
-  if (!text) {
-    return new Response("Missing ?text= parameter", { status: 400 });
-  }
-
-  try {
-    const png = await bwipjs.toBuffer({
-      bcid: "code128",
-      text,
-      scale: 3,
-      height: 10,
-      includetext: true,
-      textxalign: "center",
-    });
-
-    return new Response(png, {
-      headers: {
-        "Content-Type": "image/png",
-      },
-    });
-  } catch (err) {
-    return new Response("Error: " + err.message, { status: 500 });
-  }
+export async function onRequest() {
+  return new Response("Function is working", {
+    headers: { "Content-Type": "text/plain" },
+  });
 }
